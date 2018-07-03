6ad9985a38c39f417a752b5a0f5d47dc2cb0ac3b
alanmathew333 <mathewalan84@gmail.com>
2018-07-03 18:44:41 +0530

    client-server  connection made using socket.io

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var socket = require('socket.io')
+
 var app = express()
 
 var server = app.listen(process.env.port || 4000, () => {
@@ -6,3 +8,10 @@ var server = app.listen(process.env.port || 4000, () => {
 })
 
 app.use(express.static('./public'))
+
+
+var io = socket(server)
+
+io.on('connection', (socket) => {
+  console.log('made socket connection', socket.id)    
+})
